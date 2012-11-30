5f0d7953ca072aa2a5a929d9d3ae4f09ab9b9716
brandonpayton <brandon@happycode.net>
2012-11-29 21:03:44 -0700

    Implement client's ArticleStore put()

--- client/app/store/ArticleStore.js
+++ client/app/store/ArticleStore.js
@@ -3,6 +3,12 @@ define([
     "dojo/request",
     "dojo/store/util/QueryResults"
 ], function(declare, request, QueryResults) {
+
+    function readArticle(article) {
+        article.date = new Date(article.date);
+        return article;
+    }
+
     return declare([], {
         _feedUrl: null,
 
@@ -15,13 +21,22 @@ define([
             return object[this.idProperty];
         },
 
+        put: function(article, options) {
+            return request.put(this._feedUrl + "/article/" + encodeURIComponent(article._id), {
+                headers: {
+                    // DOJO TODO: File bug about case-sensitive Content-Type header. If the casing isn't like this, then the default content type overrides ours.
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(article),
+                handleAs: "json"
+            }).then(readArticle);
+        },
+
         query: function(query, options) {
             var asyncResults = request(this._feedUrl + "/articles", {
                 handleAs: "json"
             }).then(function(results) {
-                results.forEach(function(article) {
-                    article.date = new Date(article.date);
-                });
+                results.forEach(readArticle);
                 return results;
             });
             return QueryResults(asyncResults);
