02c579ddf46a1cba14c903a519e57a276aeb4dcb
brandonpayton <brandon@happycode.net>
2012-12-03 22:35:03 -0700

    Fix main to exit process if unable to retrieve feed list.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 define([
     "require",
+    "dojo/when",
     "dojo/node!http",
     "dojo/node!send",
     "dojo/node!url",
@@ -8,69 +9,74 @@ define([
     "dojo/store/Observable",
     "./FeedRestApi",
     "./FeedUpdater"
-], function(contextRequire, http, send, url, Sofa, FeedStore, Observable, FeedRestApi, FeedUpdater) {
+], function(contextRequire, when, http, send, url, Sofa, FeedStore, Observable, FeedRestApi, FeedUpdater) {
 
     var couchDbUrl = "http://localhost:5984/new-news",
         feedStore = Observable(new FeedStore(couchDbUrl)),
-        allFeedsQueryResults = feedStore.query(),
         feedUpdater = new FeedUpdater(feedStore),
         feedRestApi = new FeedRestApi(feedStore, feedUpdater);
 
-    allFeedsQueryResults.observe(function(feed, removedFrom, insertedInto) {
-        if(removedFrom > 0) {
-            if(insertedInto > 0) {
-                // Updated. For now, do nothing.
+    var allFeedsQueryResults = feedStore.query();
+    when(allFeedsQueryResults, function() {
+        allFeedsQueryResults.observe(function(feed, removedFrom, insertedInto) {
+            if(removedFrom > 0) {
+                if(insertedInto > 0) {
+                    // Updated. For now, do nothing.
+                } else {
+                    // Deleted. Do nothing.
+                }
             } else {
-                // Deleted. Do nothing.
+                // Added.
+                feedUpdater.updateFeed(feed);
             }
-        } else {
-            // Added.
-            feedUpdater.updateFeed(feed);
-        }
+        });
+        setTimeout(function updateFeeds() {
+            allFeedsQueryResults.forEach(feedUpdater.queueFeed.bind(feedUpdater));
+            // TODO: Need to think about how to do this better than update all at the same time.
+            
+            setTimeout(updateFeeds, 1000 * 60 * 30);
+        }, 0);
+    }, function(err) {
+        console.error("Unable to get feeds list. " + err);
+        process.exit(-1);
+    }).then(function() {
+        http.createServer(function(req, res) {
+            var u = url.parse(req.url)
+            console.log("connect", u.pathname)
+
+            function error(err) {
+                res.statusCode = err.status || 500
+                res.end(err.message)
+            }
+
+            function redirect(path) {
+                res.statusCode = 301;
+                res.setHeader('Location', path);
+                res.end('Redirecting to ' + path);
+            }
+
+            var clientUrlPattern = new RegExp("^/client|dojo/"),
+                dataUrlPattern = new RegExp("^/data(/.*)"),
+                match,
+                dataPath;
+
+            if([ "", "/", "/client" ].indexOf(u.pathname) >= 0) {
+                redirect("/client/client.html");
+            } else if(clientUrlPattern.test(u.pathname)) {
+                // Send static client resources.
+                send(req, u.pathname)
+                // TODO: How to get __dirname when using dojo loader?
+                .root(contextRequire.toUrl(".") + "/..")
+                .on('error', console.error)
+                .pipe(res);
+            } else if(match = dataUrlPattern.exec(u.pathname)) {
+                console.log("data", u.pathname);
+                dataPath = match[1];
+                feedRestApi.serve(dataPath, req, res);
+            } else {
+                res.statusCode = 404;
+                res.end(http.STATUS_CODES[404]);
+            }
+        }).listen(8080);
     });
-    setTimeout(function updateFeeds() {
-        allFeedsQueryResults.forEach(feedUpdater.queueFeed.bind(feedUpdater));
-        // TODO: Need to think about how to do this better than update all at the same time.
-        
-        setTimeout(updateFeeds, 1000 * 60 * 30);
-    }, 0);
-
-    http.createServer(function(req, res) {
-        var u = url.parse(req.url)
-        console.log("connect", u.pathname)
-
-        function error(err) {
-            res.statusCode = err.status || 500
-            res.end(err.message)
-        }
-
-        function redirect(path) {
-            res.statusCode = 301;
-            res.setHeader('Location', path);
-            res.end('Redirecting to ' + path);
-        }
-
-        var clientUrlPattern = new RegExp("^/client|dojo/"),
-            dataUrlPattern = new RegExp("^/data(/.*)"),
-            match,
-            dataPath;
-
-        if([ "", "/", "/client" ].indexOf(u.pathname) >= 0) {
-            redirect("/client/client.html");
-        } else if(clientUrlPattern.test(u.pathname)) {
-            // Send static client resources.
-            send(req, u.pathname)
-            // TODO: How to get __dirname when using dojo loader?
-            .root(contextRequire.toUrl(".") + "/..")
-            .on('error', console.error)
-            .pipe(res);
-        } else if(match = dataUrlPattern.exec(u.pathname)) {
-            console.log("data", u.pathname);
-            dataPath = match[1];
-            feedRestApi.serve(dataPath, req, res);
-        } else {
-            res.statusCode = 404;
-            res.end(http.STATUS_CODES[404]);
-        }
-    }).listen(8080);
 });
