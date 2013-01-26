ede1983964c97d721c99a3546ba000f0739e3baf
brandonpayton <brandon@happycode.net>
2013-01-26 15:24:19 -0700

    Cleaned up and commented client's main.js

--- client/app/main.js
+++ client/app/main.js
@@ -1,31 +1,9 @@
-/**
- * This file is your application’s main JavaScript file. It is listed as a dependency in run.js and will
- * automatically load when run.js loads.
- *
- * Because this file has the special filename “main.js”, and because we’ve registered the “app” package in run.js,
- * whatever object this module returns can be loaded by other files simply by requiring “app” (instead of “app/main”).
- *
- * Our first dependency is to the “dojo/has” module, which allows us to conditionally execute code based on
- * configuration settings or environmental information. Unlike a normal conditional, these branches can be compiled
- * away by the build system; see “staticHasFeatures” in app.profile.js for more information.
- *
- * Our second dependency is to the special module “require”; this allows us to make additional require calls using
- * relative module IDs within the body of our define function.
- *
- * In all cases, whatever function is passed to define() is only invoked once, and the return value is cached.
- *
- * More information about everything described about the loader throughout this file can be found at
- * http://livedocs.dojotoolkit.org/loader/amd.
- */
 define([
-    'exports',
     'require',
-    'dojo/has',
-    'dojo/_base/lang',
     './store/FeedStore',
-    './store/ArticleStore',
-    'dojo/store/Observable'
-], function (app, require, has, lang, FeedStore, ArticleStore, Observable) {
+    './store/ArticleStore'
+], function (require, FeedStore, ArticleStore) {
+    // A future version may instantiate a different UI depending on platform or form factor.
     require([ './ui/Desktop', 'dojo/domReady!' ], function (Ui) {
         var dataUrl = "../data";
         var ui = new Ui({
